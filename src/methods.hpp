2cd746cc673f17281e84c561164e2e5931d2ecb9
oToTak <t.teodorko@gmail.com>
2026-04-23 21:57:18 +0300

    initial commit

---
+++ src/methods.hpp
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include <vector>
+
+#include <cstdint>
+
+
+
+struct Stats {
+
+    size_t units = 0;
+
+    size_t points = 0;
+
+    size_t words = 0;
+
+    size_t errors = 0;
+
+};
+
+
+
+bool is_ws(uint32_t u);
+
+void process_utf8(const std::vector<uint8_t>& d, Stats& s);
+
+void process_utf16(const std::vector<uint8_t>& d, Stats& s, bool le);
+
